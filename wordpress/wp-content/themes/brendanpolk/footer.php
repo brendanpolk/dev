0260b3668bd2517fd7a151b06c9b2f811a298bd8
Brendan Polk <brendanpolk@gmail.com>
2014-03-11 15:54:04 -0500

    further theme integration
    moved style.css and image folder into theme, copied markup into
    header.php, footer.php, and index.php

--- wordpress/wp-content/themes/brendanpolk/footer.php
+++ wordpress/wp-content/themes/brendanpolk/footer.php
@@ -14,6 +14,18 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
+			<div class="footer"><!-- BEGIN COPIED FOOTER -->
+	            <div class="footer-right">
+	    		  <p>&copy Brendan Polk. 2014. All Rights Reserved</p>
+	            </div>
+	            <div class="footer-left">
+	                <a href="http://dribbble.com/brendanpolk"><img src="/img/social/dribbble.png"></a>
+	                <a href="http://twitter.com/brendanpolk"><img src="/img/social/twitter.png"></a>
+	                <a href="http://www.linkedin.com/in/brendanpolk"><img src="/img/social/linkedin.png"></a>
+	                 <a href="https://github.com/brendanpolk"><img src="/img/social/github.png"></a>
+	            </div>
+	    	</div><!-- END FOOTER -->
+
 			<?php get_sidebar( 'footer' ); ?>
 
 			<div class="site-info">
--- wordpress/wp-content/themes/brendanpolk/index.php
+++ wordpress/wp-content/themes/brendanpolk/index.php
@@ -18,6 +18,164 @@ get_header(); ?>
 
 <div id="main-content" class="main-content">
 
+
+        <div id="hero"><!-- BEGIN COPIED BODY CONTENT -->
+            <h1>Brendan Polk</h1><p>Web & Graphic Design</p>
+        </div>
+
+	   <div id="grid-container"><!-- BEGIN GRID SECTION -->
+
+            <div class="grid-item-container">
+                <a href="welcomewebstudios.html"></a>
+                <div class="grid-item">
+                    <img src="img/work/wws/welcome-thumb.png">
+                    <div class="item-title">
+                        <a href="welcomewebstudios.html"><h1>Welcome Web Studios</h1></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <a href="fixed-4-lyfe.html"></a>
+                    <div class="grid-item">
+                        <a href="fixed-4-lyfe.html">
+                        <img src="img/work/fixed4lyfe/fixed4life.png"></a>
+                    </div>
+                <div class="item-title">
+                    <a href="fixed-4-lyfe.html"><h1>Fixed 4 Lyfe</h1></a>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <a href="armadillo-ale.html">
+                    <img src="img/work/armadillo/armadillo-ale-poster.png"></a>
+                </div>
+                <div class="item-title">
+                    <a href="armadillo-ale.html"><h1>Armadillo Ale</h1></a>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg"> 
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/2.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+            <div class="grid-item-container">
+                <div class="grid-item">
+                    <img src="img/3.jpg">
+                </div>
+                <div class="item-title">
+                    <h1>Welcome Web Studios</h1>
+                </div>
+            </div>
+
+        </div><!-- END GRID SECTION --><!--END BODY CONTENT SECTION -->
+
+
 <?php
 	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
 		// Include the featured content template.
